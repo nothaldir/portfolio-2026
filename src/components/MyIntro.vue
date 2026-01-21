ecbb77ded8518bb1efe727e7e94e49b13817264a
Alex Berneau <haldir.alex@gmail.com>
2026-01-21 16:03:51 +0100

    update copy & fix responsive

--- src/components/MyIntro.vue
+++ src/components/MyIntro.vue
@@ -1,13 +1,13 @@
 <template>
   <section>
     <p>
-      Welcome,
+      Hi there 👋,
       <br />
       I'm Alex, a Front-End Engineer based in Paris, France.
     </p>
     <p>
-      I like working with modern JavaScript and TypeScript tools: Vue, Nuxt, React, Next, Supabase
-      and many more.
+      I like working with modern JavaScript and TypeScript tools: Vue, Nuxt, Next and Supabase are
+      just a few of them.
       <br />
       Through my work, I try to have a positive impact on both the people I work with and those who
       use what I build.
@@ -15,7 +15,7 @@
     <p>
       Want to know a bit more?
       <br />
-      You can find some more information below, but you can also reach out to me via the
+      You can find more information below, and you can also reach out to me via the
       <a href="#links">links here</a> 👇.
     </p>
     <p>Thanks for stopping by!</p>
